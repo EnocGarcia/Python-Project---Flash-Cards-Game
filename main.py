2114b7dd8ffebc0cb1fa2870039000f5339f2a6a
Enoc García <enocgarcia21@gmail.com>
2022-01-22 23:06:55 -0300

    setting flash timer and flip

--- main.py
+++ main.py
@@ -1,23 +1,50 @@
 """
 Flash Card Game Project
 """
+
 from tkinter import Tk, Canvas, PhotoImage, Button
 import random
-from webbrowser import get
 import pandas as pd
 
 # --------------------- FUNCTIONS ---------------------
 
 dataset = pd.read_csv("data/french_words.csv")
+
+
 words_dict = dataset.to_dict(orient="records")
-word = random.choice(words_dict)["French"]
+
+front_language = dataset.columns[0]
+back_language = dataset.columns[1]
+
+CURRENT_WORD = {}
 
 
 def get_word():
     """Gets a new french word from the buttons"""
-    new_word = random.choice(words_dict)["French"]
-    flash_card.itemconfig(card_title, text="French")
-    flash_card.itemconfig(card_word, text=f"{new_word}")
+    global CURRENT_WORD, FLIP
+
+    CURRENT_WORD = random.choice(words_dict)
+
+    window.after_cancel(FLIP)
+
+    flash_card.itemconfig(card_title, text=f"{front_language}", fill="black")
+    flash_card.itemconfig(
+        card_word, text=f"{CURRENT_WORD[f'{front_language}']}", fill="black"
+    )
+    flash_card.itemconfig(card_image, image=card_front)
+
+    FLIP = window.after(3000, flip_card)
+
+
+def flip_card():
+    """Flips the card to the back language"""
+    global CURRENT_WORD
+
+    flash_card.itemconfig(card_image, image=card_back)
+    flash_card.itemconfig(card_title, text=f"{back_language}", fill="white")
+    flash_card.itemconfig(
+        card_word, text=f"{CURRENT_WORD[f'{back_language}']}", fill="white"
+    )
 
 
 # ---------------------- UI SETUP ----------------------
@@ -27,15 +54,19 @@ window = Tk()
 window.title("Flash Card Game")
 window.config(background=BACKGROUND_COLOR, padx=50, pady=50)
 
+
+FLIP = window.after(3000, flip_card)
+
 flash_card = Canvas(
     width=800, height=523, highlightthickness=0, background=BACKGROUND_COLOR
 )
 card_front = PhotoImage(file="images/card_front.png")
-flash_card.create_image(400, 263, image=card_front)
+card_back = PhotoImage(file="images/card_back.png")
+card_image = flash_card.create_image(400, 263, image=card_front)
 card_title = flash_card.create_text(
     400, 150, text="Language", font=("Arial", 40, "italic")
 )
-card_word = flash_card.create_text(400, 263, text=f"word", font=("Arial", 60, "bold"))
+card_word = flash_card.create_text(400, 263, text="word", font=("Arial", 60, "bold"))
 flash_card.grid(column=0, columnspan=2, row=0)
 
 correct_image = PhotoImage(file="images/right.png")
@@ -49,4 +80,5 @@ wrong_button.config(command=get_word)
 wrong_button.grid(column=1, row=1)
 
 get_word()
+
 window.mainloop()
